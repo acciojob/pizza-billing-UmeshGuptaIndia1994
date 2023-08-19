cdc97b1d6098f1c8617ca3e3f1fd6006b93c9949
Umesh Gupta <136713880+UmeshGuptaIndia1994@users.noreply.github.com>
2023-08-19 18:13:51 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 
-class Main {
-  public static void main(String[] args) {
-    Pizza p = new Pizza(true);
-    System.out.println(p.getPrice());
-    p.addTakeaway();
-    p.addExtraCheese();
-    p.addTakeaway();
-    System.out.println(p.getBill());
+public class Main {
+    public static void main(String[] args) {
+        DeluxePizza dp = new DeluxePizza(true);
+        System.out.println(dp.getPrice());
+        dp.addTakeaway();
+        dp.addExtraCheese();
+        dp.addTakeaway();
+        System.out.println(dp.getBill());
 
-    DeluxePizza dp = new DeluxePizza(true);
-    dp.addExtraCheese();
-    dp.addExtraToppings();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
-  }
+        Pizza p = new Pizza(false);
+        p.addExtraCheese();
+        p.addExtraToppings();
+        System.out.println(p.getBill());
+    }
 }
