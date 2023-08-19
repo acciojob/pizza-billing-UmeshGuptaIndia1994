efb3a61e059c7fb6a36ba792c219753870f688d4
Umesh Gupta <136713880+UmeshGuptaIndia1994@users.noreply.github.com>
2023-08-19 17:34:21 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,17 @@
 
 class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
-    dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
-
     Pizza p = new Pizza(true);
+    System.out.println(p.getPrice());
+    p.addTakeaway();
     p.addExtraCheese();
-    p.addExtraToppings();
     p.addTakeaway();
     System.out.println(p.getBill());
+
+    DeluxePizza dp = new DeluxePizza(true);
+    dp.addExtraCheese();
+    dp.addExtraToppings();
+    dp.addTakeaway();
+    System.out.println(dp.getBill());
   }
 }
